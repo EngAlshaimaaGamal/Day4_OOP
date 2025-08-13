27d7c5cb5eba526a1cf54780d52f34d53e3e696d
Microsoft <Microsoft@ALSHAIMAA>
2025-08-13 18:04:52 +0300

    operator overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Demo
+﻿using System.Numerics;
+using Demo.overload_Examles;
+
+
+namespace Demo
 {
     internal class Program
     {
@@ -47,12 +51,40 @@
         static void Main(string[] args)
         {
 
+            #region Overloading
+
+            #region Methods Overloading
 
             //Console.WriteLine(SumNumbers(5, 6));
             //Console.WriteLine(SumNumbers(5.5, 6.5));    
             //Console.WriteLine(SumNumbers(5.5, 6));  
             //Console.WriteLine(SumNumbers(5, 6, 7)); 
             //Console.WriteLine(SumNumbers(5.5, 6, 7.5)); 
+
+
+
+            #endregion
+
+            #region  operator Overloading
+
+            // MyComplex c1 = new MyComplex () { Real = 3, Img = 4 }; 
+
+            //Console.WriteLine(c1); // Output: 3 + 4i
+
+            //MyComplex c2 = new MyComplex() { Real = 7, Img = 8 }; 
+             
+            //Console.WriteLine(c2); // Output: 7 + 8i
+            //                       // 
+            //Console.WriteLine("============================");
+            //MyComplex c3 = c1 + c2;
+            //Console.WriteLine(c3); // Output: 10 + 12i
+
+
+            #endregion
+
+
+            #endregion
+
 
 
 
---
+++ Demo/overload_Examles/Complex.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.overload_Examles
+{
+    public class MyComplex
+    {
+
+        #region Properities
+
+        public double Real { get; set; }
+        public double Img { get; set; }
+
+
+        #endregion
+        #region Constructor
+
+        //public MyComplex(double real, double img)
+        //{
+        //    Real = real;
+        //    Img = img;
+        //}
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i";
+        }
+
+
+        #endregion
+
+
+        #region operators
+
+        public static MyComplex operator + (MyComplex left, MyComplex right)
+        {
+
+            return new MyComplex()
+            {
+                Real = (left?.Real ?? 0) + (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) + (right?.Img ?? 0)
+
+
+            };
+
+            //MyComplex result = new MyComplex( )
+            //{
+            //    Real = (left?.Real ?? 0)+ (right?.Real ?? 0),
+            //    Img = (left?.Img ?? 0) + ( right?.Img??0 )
+
+
+            //};
+            // return result;
+
+
+
+
+        }
+
+        public static MyComplex operator - (MyComplex left, MyComplex right)
+        {
+
+            return new MyComplex()
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) - (right?.Img ?? 0)
+
+
+            }; // more readiable way to write it    
+
+            //MyComplex result = new MyComplex( )
+            //{
+            //    Real = (left?.Real ?? 0)- (right?.Real ?? 0),
+            //    Img = (left?.Img ?? 0) - ( right?.Img??0 )
+
+
+            //};
+            // return result;
+
+
+
+
+        }
+
+
+        #endregion
+
+
+    }
+}
